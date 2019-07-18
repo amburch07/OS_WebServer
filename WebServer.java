5250cb3ffb1f8f699ea4e42836bc9becf4ce9b88
Arianna Burch <amburch07@gmail.com>
2019-07-18 10:52:51 -0400

    Add files via upload

---
+++ WebServer.java
@@ -0,0 +1,28 @@
+import java.net.*;
+
+public final class WebServer {
+	public static void main(String argv[]) throws Exception {
+		// Get the port number from the command line.
+		int port = 1234;
+
+		// Establish the listen socket.
+		@SuppressWarnings("resource")
+		ServerSocket socket = new ServerSocket(port);
+		System.out.println("Waiting for connections...");
+		
+		// Process HTTP service requests in an infinite loop.
+		while (true) {
+			// Listen for a TCP connection request.
+			Socket connection = socket.accept();
+						
+			// Construct an object to process the HTTP request message.
+			HttpRequest request = new HttpRequest(connection);
+
+			// Create a new thread to process the request.
+			Thread thread = new Thread(request);
+
+			// Start the thread.
+			thread.start();
+		}
+	}
+}
